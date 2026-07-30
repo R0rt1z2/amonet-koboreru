b4a8d75fb65fe7d00e1bbc2421fc337bf77efbb7
R0rt1z2 <roger@r0rt1z2.com>
2026-07-30 22:26:29 +0200

    Simplify Preloader function thunks
    Change-Id: I4419c60a07d258777ad563b8f8ad3f4994cd066f

--- include/preloader.h
+++ include/preloader.h
@@ -1,17 +1,11 @@
 #pragma once
 
+#include <stdint.h>
+
 #include "device.h"
 
-typedef int (*boot_device_init_fn)(void);
-static inline int boot_device_init(void)
-{
-    boot_device_init_fn fn = (boot_device_init_fn)(uintptr_t)(BDEV_INIT_ADDR | 1);
-    return fn();
-}
+static int (*const boot_device_init)(void) = (void *)(BDEV_INIT_ADDR | 1);
 
-typedef int (*bldr_load_part_fn)(char *name, void *bdev, uint32_t *addr, uint32_t *size);
-static inline int bldr_load_part(char *name, void *bdev, uint32_t *addr, uint32_t *size)
-{
-    bldr_load_part_fn fn = (bldr_load_part_fn)(uintptr_t)(BLDR_LOAD_PART_ADDR | 1);
-    return fn(name, bdev, addr, size);
-}
+static int (*const bldr_load_part)(char *name, void *bdev, uint32_t *addr,
+                                   uint32_t *size) =
+        (void *)(BLDR_LOAD_PART_ADDR | 1);
