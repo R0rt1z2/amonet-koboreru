9d97abdc3943bdc0e5ba918ce175e0bc076ffcb3
Ben Grisdale <bengris32@protonmail.ch>
2026-08-03 18:49:43 +0100

    Add support for Echo Dot 3rd gen refresh (crumpet)
    This one has NAND storage, god bless.
    
    Change-Id: Id77ce74c6e632ea9159463fbec11f376924fbff9
    Signed-off-by: Ben Grisdale <bengris32@protonmail.ch>

---
+++ devices/crumpet.c
@@ -0,0 +1,28 @@
+#include <device.h>
+#include <patch.h>
+
+#include <drivers/kpd.h>
+
+void apply_patches(void)
+{
+    // Ensure we can load unsigned DAs
+    patch_ret(0x00217F2C, 0);
+
+    // Do not override USB descriptors with the originals.
+    patch_word(0x00217548, 0xBF00447D);
+
+    // Make sure the ARB check never runs (seriously,
+    // ARB on this device USES FUSES).
+    patch_ret(0x002019B0, 0);
+
+    // This function is used to burn the new ARB version
+    // to the devices fuses. Patch it to never run too,
+    // just to be safe.
+    patch_ret(0x00201954, 0);
+}
+
+uint8_t usbdl_detect_key(void)
+{
+    // Force hacked USBDL for now.
+    return 1;
+}
