09b14458268ce16b469d1e2b8b2c96a197175b42
Ben Grisdale <bengris32@protonmail.ch>
2026-08-04 20:57:09 +0100

    Add support for Fire HD 8 7th Generation (douglas)
    Sorry k4y0z
    
    Change-Id: Iaccb78737688b792721e2d66acd66e41e092ddca
    Signed-off-by: Ben Grisdale <bengris32@protonmail.ch>

---
+++ devices/douglas.c
@@ -0,0 +1,21 @@
+#include <device.h>
+#include <patch.h>
+
+#include <drivers/pmic_keys.h>
+
+void apply_patches(void)
+{
+    // Ensure we can load unsigned DAs
+    patch_ret(0x0021B970, 0);
+
+    // Do not override USB descriptors with the originals.
+    patch_word(0x00217420, 0xBF004628);
+
+    // Make sure the ARB check never runs.
+    patch_ret(0x00201934, 0);
+}
+
+uint8_t usbdl_detect_key(void)
+{
+    return pmic_key_pressed(USBDL_PMIC_KEY);
+}
