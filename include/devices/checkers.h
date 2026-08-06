a681858c082f2fdf649fe3d72bf1d51b263c89a3
R0rt1z2 <roger@r0rt1z2.com>
2026-08-07 01:26:11 +0200

    Load LK from expdb for checkers and crown
    Change-Id: I3d0429e00a7393465476a24e154160bc15ffa69f

--- include/devices/checkers.h
+++ include/devices/checkers.h
@@ -8,7 +8,7 @@
 #define SRAM_DBUF_PTR 0x0010298C
 #define DRAM_BUF      0x4BE00000
 
-#define LK_PART_NAME  "swdl"
+#define LK_PART_NAME  "expdb"
 #define LK_LOAD_ADDR  0x4BD00000
 
 #define BDEV_ADDR      0x00102DC0
