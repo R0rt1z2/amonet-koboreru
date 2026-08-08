cc6adaf81a84c2f1c7cb8a90fac436c4976a9d4c
Ben Grisdale <bengris32@protonmail.ch>
2026-08-08 21:43:14 +0100

    Add patch_branch helper
    Change-Id: I7e48eb2da43ab3c5cb4a73b39db0228d5eb7bd4e
    Signed-off-by: Ben Grisdale <bengris32@protonmail.ch>

--- include/patch.h
+++ include/patch.h
@@ -4,3 +4,4 @@
 
 void patch_word(uint32_t addr, uint32_t value);
 void patch_ret(uint32_t addr, uint32_t value);
+void patch_branch(uint32_t addr, const void *target);
--- patch.c
+++ patch.c
@@ -23,3 +23,22 @@ void patch_ret(uint32_t addr, uint32_t value)
     patch_word(addr, (THUMB_BX_LR << 16) | THUMB_LDR_R0);
     patch_word(addr + 4, value);
 }
+
+void patch_branch(uint32_t addr, const void *target)
+{
+    int32_t off = (int32_t)(((uint32_t)target & ~1u) - (addr + 4));
+    uint32_t u = (uint32_t)off;
+
+    uint32_t s     = (u >> 24) & 1;
+    uint32_t i1    = (u >> 23) & 1;
+    uint32_t i2    = (u >> 22) & 1;
+    uint32_t j1    = (~(i1 ^ s)) & 1;
+    uint32_t j2    = (~(i2 ^ s)) & 1;
+    uint32_t imm10 = (u >> 12) & 0x3FF;
+    uint32_t imm11 = (u >> 1) & 0x7FF;
+
+    uint16_t hi = (uint16_t)(0xF000 | (s << 10) | imm10);
+    uint16_t lo = (uint16_t)(0x9000 | (j1 << 13) | (j2 << 11) | imm11);
+
+    patch_word(addr, ((uint32_t)lo << 16) | hi);
+}
