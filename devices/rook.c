fb23eab63ad3e2312b6a9971c1a54b5eafe67c50
R0rt1z2 <roger@r0rt1z2.com>
2026-08-16 16:12:21 +0200

    Require power + volume-down for USBDL on rook
    Change-Id: I2f22940b366df01f727065f544d633c72db60b0f

--- devices/rook.c
+++ devices/rook.c
@@ -2,6 +2,8 @@
 #include <patch.h>
 #include <tee.h>
 
+#include <drivers/gpio.h>
+#include <drivers/timer.h>
 #include <drivers/pmic_keys.h>
 
 void apply_patches(void)
@@ -19,7 +21,20 @@ void apply_patches(void)
     patch_branch(0x0020A32C, bldr_load_tee_part);
 }
 
+static uint8_t key_pressed(uint32_t pin)
+{
+    mt_set_gpio_mode(pin, GPIO_MODE_GPIO);
+    mt_set_gpio_dir(pin, GPIO_DIR_IN);
+    mt_set_gpio_ies(pin, GPIO_IES_ENABLE);
+    mt_set_gpio_pull(pin, GPIO_PULL_ENABLE, GPIO_PULL_UP);
+
+    mdelay(1);
+
+    return mt_get_gpio_in(pin) == 0;
+}
+
 uint8_t usbdl_detect_key(void)
 {
-    return pmic_key_pressed(USBDL_PMIC_KEY);
+    return pmic_key_pressed(USBDL_PMIC_KEY)
+        && key_pressed(VOL_DOWN_GPIO);
 }
