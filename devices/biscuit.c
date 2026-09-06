a81622159c31ffa9c985cf6fc2397f87070750b5
R0rt1z2 <roger@r0rt1z2.com>
2026-09-06 16:56:16 +0200

    Update biscuit support to latest Preloader
    Change-Id: I15e633658fae3070da92b8f6c326ce15ed55647c

--- devices/biscuit.c
+++ devices/biscuit.c
@@ -1,3 +1,4 @@
+#include <arb.h>
 #include <device.h>
 #include <patch.h>
 #include <tee.h>
@@ -7,13 +8,13 @@
 void apply_patches(void)
 {
     // Ensure we can load unsigned DAs
-    patch_ret(0x0021C5E4, 0);
+    patch_ret(0x0021C1BC, 0);
 
     // Do not override USB descriptors with the originals.
-    patch_word(0x00218094, 0xBF004628);
+    patch_word(0x00217C6C, 0xBF004628);
 
-    // Make sure the ARB check never runs.
-    patch_ret(0x00201934, 0);
+    // Replace the ARB check with a routine that clears the counters.
+    patch_branch(0x00201934, clear_rpmb_arb);
 
     // Replace the TEE image loader with our own
     patch_branch(0x0020A910, bldr_load_tee_part);
